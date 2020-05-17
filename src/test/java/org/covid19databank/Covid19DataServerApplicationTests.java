b36f93ee0d3cc58a0e8b5485abc6ae4fc7ecefea
Vicad006 <vicad006@yaoo.com>
2020-05-17 20:40:08 +0100

    Initialize Project

---
+++ src/test/java/org/covid19databank/Covid19DataServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.covid19databank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Covid19DataServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
