920194571db01ff7dde7c68d5556f62fb7db56ff
vicad006 <vicad006@yahoo.com>
2020-05-17 21:53:11 +0100

    added configuration properties

--- src/main/java/org/covid19databank/Application.java
+++ src/main/java/org/covid19databank/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Covid19DataServerApplication {
+public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(Covid19DataServerApplication.class, args);
+        SpringApplication.run(Application.class, args);
     }
 
 }
--- src/test/java/org/covid19databank/ApplicationTests.java
+++ src/test/java/org/covid19databank/ApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class Covid19DataServerApplicationTests {
+class ApplicationTests {
 
     @Test
     void contextLoads() {
