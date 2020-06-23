598d3cfd3e46f0609177f38484c87abbcf8c522e
Vicad006 <vicad006@yaoo.com>
2020-06-23 21:32:14 +0100

    Called the getLiteratureData method from the LiteratureLoaderService in the initDB class to initalize it.

--- src/main/java/org/covid19databank/loader/InitDB.java
+++ src/main/java/org/covid19databank/loader/InitDB.java
@@ -2,6 +2,7 @@ package org.covid19databank.loader;
 
 import org.covid19databank.services.DataLoaderService;
 import org.covid19databank.services.InitDataBaseService;
+import org.covid19databank.services.LiteratureLoaderService;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -12,22 +13,26 @@ public class InitDB implements CommandLineRunner {
 
     private DataLoaderService dataLoaderService;
     private InitDataBaseService initDataBaseService;
+    private LiteratureLoaderService literatureLoaderService;
 
     @Value("${init.database}")
     private boolean iniitializeDatabase;
 
-    public InitDB(DataLoaderService dataLoaderService, InitDataBaseService initDataBaseService) {
+    public InitDB(DataLoaderService dataLoaderService, InitDataBaseService initDataBaseService, LiteratureLoaderService literatureLoaderService) {
         this.dataLoaderService = dataLoaderService;
         this.initDataBaseService = initDataBaseService;
+        this.literatureLoaderService = literatureLoaderService;
     }
 
     @Override
     public void run(String... args) throws Exception {
         if (iniitializeDatabase) {
             initDataBaseService.loadRegions();
+            initDataBaseService.loadLiteratureTypes();
            // initDataBaseService.loadCountries();
         }
 
         dataLoaderService.getCasesData();
+        literatureLoaderService.getLiteratureData();
     }
 }
