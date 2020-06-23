2ad8ef1519a80a2dbd9fb1d7d2573d78cc24cea0
Vicad006 <vicad006@yaoo.com>
2020-06-23 21:09:43 +0100

    Created DTO for publication

---
+++ src/main/java/org/covid19databank/payload/europepmc/Publication.java
@@ -0,0 +1,34 @@
+package org.covid19databank.payload.europepmc;
+
+import java.util.List;
+
+public class Publication {
+
+    private int hitCount;
+    private List<Entry> entries;
+    private List facets;
+
+    public int getHitCount() {
+        return hitCount;
+    }
+
+    public void setHitCount(int hitCount) {
+        this.hitCount = hitCount;
+    }
+
+    public List<Entry> getEntries() {
+        return entries;
+    }
+
+    public void setEntries(List<Entry> entries) {
+        this.entries = entries;
+    }
+
+    public List getFacets() {
+        return facets;
+    }
+
+    public void setFacets(List facets) {
+        this.facets = facets;
+    }
+}
