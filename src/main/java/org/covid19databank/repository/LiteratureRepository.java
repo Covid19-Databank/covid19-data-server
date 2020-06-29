f74fcf589db5a03a39ce87c2fae325655abd54fa
Vicad006 <vicad006@yaoo.com>
2020-06-29 22:37:39 +0100

    Changed the id datatype from int to Integer

--- src/main/java/org/covid19databank/repository/LiteratureRepository.java
+++ src/main/java/org/covid19databank/repository/LiteratureRepository.java
@@ -2,7 +2,9 @@ package org.covid19databank.repository;
 
 import org.covid19databank.datamodel.Literature;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface LiteratureRepository extends JpaRepository<Literature, Integer> {
 
 
