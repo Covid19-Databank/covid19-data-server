39274e9cc2e7bfb5e1ba91ee55a494e248ac703f
Vicad006 <vicad006@yaoo.com>
2020-06-29 22:39:25 +0100

    Created GeneralEnum class for general enums

--- src/main/java/org/covid19databank/services/constant/GeneralEnum.java
+++ src/main/java/org/covid19databank/services/constant/GeneralEnum.java
@@ -2,7 +2,7 @@ package org.covid19databank.services.constant;
 
 public enum GeneralEnum {
 
-    url("https://www.ebi.ac.uk/ebisearch/ws/rest/complex-portal/?query=TAXONOMY:(2697049 OR 694009)&size=15&format=JSON&facetcount=10&fieldurl=true&viewurl=true&fields=name,description,organism,curated-complex&entryattrs=score");
+    Target_URL("https://www.ebi.ac.uk/ebisearch/ws/rest/complex-portal/?query=TAXONOMY:(2697049 OR 694009)&size=15&format=JSON&facetcount=10&fieldurl=true&viewurl=true&fields=name,description,organism,curated-complex&entryattrs=score");
 
 
     private String value;
