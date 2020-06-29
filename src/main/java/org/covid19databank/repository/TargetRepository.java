49ce34dcd46f11c0a7e5e60f9374705143fbcb19
Vicad006 <vicad006@yaoo.com>
2020-06-29 22:35:59 +0100

    Created TargetRepository

--- src/main/java/org/covid19databank/repository/TargetRepository.java
+++ src/main/java/org/covid19databank/repository/TargetRepository.java
@@ -2,6 +2,9 @@ package org.covid19databank.repository;
 
 import org.covid19databank.datamodel.Target;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface TargetRepository extends JpaRepository<Target, Integer> {
+
 }
