676863c56ea7b751741939cc0c23d7aa9a439af2
Vicad006 <vicad006@yaoo.com>
2020-07-08 15:20:06 +0100

    Commented out SINGLE_CELL_EXPRESSION_EXPERIMENT and SINGLE_CELL_EXPRESSION

--- src/main/java/org/covid19databank/services/constant/ExpressionTypeEnum.java
+++ src/main/java/org/covid19databank/services/constant/ExpressionTypeEnum.java
@@ -5,11 +5,13 @@ import java.util.Map;
 
 public enum ExpressionTypeEnum {
 
-    GENES_RECEPTORS_AND_ANTIBODIES("Genes, Receptors And Antibodies", "https://www.ebi.ac.uk/ebisearch/ws/rest/europepmc/?query=\"coronavirus spike protein\" OR \"angiotensin-converting enzyme 2\" OR TMPRSS2&size=100&sort=publication_date:descending,_relevance:descending&format=JSON&facetcount=10&fieldurl=true&viewurl=true&fields=name,author,display_author,publication_date,journal,volume,issue,pagination,pmcid,ORCID,source&entryattrs=score"),
+    GENES_EXPRESSION_EXPERIMENT("Genes Expression Experiment", "https://www.ebi.ac.uk/ebisearch/ws/rest/atlas-experiments/?query=coronavirus OR SARS OR coronaviruses OR icSARS OR id:(E-GEOD-27973 OR E-GEOD-34205 OR E-GEOD-36287 OR E-GEOD-52405)&size=15&format=JSON&facetcount=10&fieldurl=true&viewurl=true&fields=name,description,publication_date,TAXONOMY&entryattrs=score&facetfields=publication_date,TAXONOMY"),
 
     GENE_EXPRESSION("Gene Expression", "https://www.ebi.ac.uk/ebisearch/ws/rest/atlas-genes/?query=name:(ACE2^1.1 OR TMPRSS2^1.1 OR CTSB OR CTSL) TAXONOMY:9606&size=15&format=JSON&facetcount=10&fieldurl=true&viewurl=true&fields=name,description,TAXONOMY&entryattrs=score"),
 
-    SINGLE_CELL_EXPRESSION_EXPERIMENT("Single Cell Expression Experiment", "https://www.ebi.ac.uk/ebisearch/ws/rest/pride/?query=curator_keywords%3ASars-cov-2&size=15&format=JSON&facetcount=10&fieldurl=true&viewurl=true&fields=name%2Cdescription%2Cspecies%2Cpublication_date&entryattrs=score&facetfields=disease%2Ctissue%2Cpublication_date%2Cdomain_source%2CTAXONOMY"),
+    //SINGLE_CELL_EXPRESSION_EXPERIMENT("Single Cell Expression Experiment", ""),
+
+    //SINGLE_CELL_EXPRESSION("Single Cell Expression", ""),
 
     PROTEIN_EXPRESSION_EXPERIMENT("Protein Expression Experiment", "https://www.ebi.ac.uk/ebisearch/ws/rest/pride/?query=curator_keywords:Sars-cov-2&size=15&format=JSON&facetcount=10&fieldurl=true&viewurl=true&fields=name,description,species,publication_date&entryattrs=score&facetfields=disease,tissue,publication_date,domain_source,TAXONOMY");
 
