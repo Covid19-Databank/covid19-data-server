cfb03a6f5f3fbf7030e472560392deae263bdb01
Vicad006 <vicad006@yaoo.com>
2020-07-08 15:36:11 +0100

    Brought in the initDabaseService and the expressionLoaderService into the initDB and also called the loadExpressionTypes from the initDatabase and getExpressionData method from the expressionLoaderService

--- src/main/java/org/covid19databank/loader/InitDB.java
+++ src/main/java/org/covid19databank/loader/InitDB.java
@@ -1,9 +1,6 @@
 package org.covid19databank.loader;
 
-import org.covid19databank.services.DataLoaderService;
-import org.covid19databank.services.InitDataBaseService;
-import org.covid19databank.services.LiteratureLoaderService;
-import org.covid19databank.services.TargetLoaderService;
+import org.covid19databank.services.*;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -16,15 +13,17 @@ public class InitDB implements CommandLineRunner {
     private InitDataBaseService initDataBaseService;
     private LiteratureLoaderService literatureLoaderService;
     private TargetLoaderService targetLoaderService;
+    private ExpressionLoaderService expressionLoaderService;
 
     @Value("${init.database}")
     private boolean iniitializeDatabase;
 
-    public InitDB(DataLoaderService dataLoaderService, InitDataBaseService initDataBaseService, LiteratureLoaderService literatureLoaderService, TargetLoaderService targetLoaderService) {
+    public InitDB(DataLoaderService dataLoaderService, InitDataBaseService initDataBaseService, LiteratureLoaderService literatureLoaderService, TargetLoaderService targetLoaderService, ExpressionLoaderService expressionLoaderService) {
         this.dataLoaderService = dataLoaderService;
         this.initDataBaseService = initDataBaseService;
         this.literatureLoaderService = literatureLoaderService;
         this.targetLoaderService = targetLoaderService;
+        this.expressionLoaderService = expressionLoaderService;
     }
 
     @Override
@@ -32,12 +31,15 @@ public class InitDB implements CommandLineRunner {
         if (iniitializeDatabase) {
             initDataBaseService.loadRegions();
             initDataBaseService.loadLiteratureTypes();
+            initDataBaseService.loadExpressionTypes();
            // initDataBaseService.loadCountries();
         }
 
         dataLoaderService.getCasesData();
         literatureLoaderService.getLiteratureData();
         targetLoaderService.getTargetData();
+        expressionLoaderService.getExpressionData();
+
 
     }
 }
