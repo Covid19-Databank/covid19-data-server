4de2f9728ed16319b04a28861e6d95a05d946c4c
Vicad006 <vicad006@yaoo.com>
2020-07-08 15:28:14 +0100

    Created the loadExpressionTypes method

--- src/main/java/org/covid19databank/services/InitDataBaseService.java
+++ src/main/java/org/covid19databank/services/InitDataBaseService.java
@@ -4,12 +4,11 @@ import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.covid19databank.datamodel.Country;
+import org.covid19databank.datamodel.ExpressionType;
 import org.covid19databank.datamodel.LiteratureType;
 import org.covid19databank.datamodel.Region;
-import org.covid19databank.repository.CountryRepository;
-import org.covid19databank.repository.LiteratureRepository;
-import org.covid19databank.repository.LiteratureTypeRepository;
-import org.covid19databank.repository.RegionRepository;
+import org.covid19databank.repository.*;
+import org.covid19databank.services.constant.ExpressionTypeEnum;
 import org.covid19databank.services.constant.LiteratureTypeEnum;
 import org.covid19databank.services.constant.Location;
 import org.covid19databank.services.constant.Regions;
@@ -32,26 +31,29 @@ public class InitDataBaseService {
     private RegionRepository regionRepository;
     private CountryRepository countryRepository;
     private LiteratureTypeRepository literatureTypeRepository;
+    private ExpressionTypeRepository expressionTypeRepository;
 
-    public InitDataBaseService(RegionRepository regionRepository, CountryRepository countryRepository, LiteratureTypeRepository literatureTypeRepository) {
+    public InitDataBaseService(RegionRepository regionRepository, CountryRepository countryRepository, LiteratureTypeRepository literatureTypeRepository, ExpressionTypeRepository expressionTypeRepository) {
         this.regionRepository = regionRepository;
         this.countryRepository = countryRepository;
         this.literatureTypeRepository = literatureTypeRepository;
+        this.expressionTypeRepository = expressionTypeRepository;
 
     }
 
-    public void loadRegions(){
+    public void loadRegions() {
         List<Region> regions = new ArrayList<>();
         Arrays.asList(Regions.values()).forEach(regionData -> regions.add(new Region(regionData.get())));
-        if (regionRepository.count() == 0){
+        if (regionRepository.count() == 0) {
             regionRepository.saveAll(regions);
         }
     }
 
     public void loadCountries() {
         JsonNode jsonNode = readJsonLocal(Location.COUNTRIES_URL.get());
-        List<Country> countries = mapper.convertValue(jsonNode.get("data"), new TypeReference<List<Country>>(){});
-        if (countryRepository.count() == 0){
+        List<Country> countries = mapper.convertValue(jsonNode.get("data"), new TypeReference<List<Country>>() {
+        });
+        if (countryRepository.count() == 0) {
             countryRepository.saveAll(countries);
         }
     }
@@ -61,35 +63,52 @@ public class InitDataBaseService {
         try {
             BufferedReader br = new BufferedReader(new FileReader(jsonFileLink));
             jsonNode = mapper.readTree(br);
-        }catch (Exception e) {
+        } catch (Exception e) {
             log.warn(e.getMessage());
         }
         return jsonNode;
     }
 
-    public void loadLiteratureTypes(){
+    public void loadLiteratureTypes() {
 
         List<LiteratureType> literatureTypes = new ArrayList<>();
 
         List<LiteratureTypeEnum> types = Arrays.asList(LiteratureTypeEnum.values());
 
-        for(LiteratureTypeEnum type : types){
+        for (LiteratureTypeEnum type : types) {
             LiteratureType literatureType = new LiteratureType(type.getType());
             literatureTypes.add(literatureType);
         }
 
         try {
             literatureTypeRepository.saveAll(literatureTypes);
-        }catch (Exception e){
+        } catch (Exception e) {
             log.info("Duplicate Not Allowed");
         }
+    }
 
 
+    public void loadExpressionTypes() {
+
+        List<ExpressionType> expressionTypes = new ArrayList<>();
+
+        List<ExpressionTypeEnum> expressionTypeEnumTypes = Arrays.asList(ExpressionTypeEnum.values());
+
+        for (ExpressionTypeEnum expressionTypeEnumType : expressionTypeEnumTypes) {
+            ExpressionType expressionType = new ExpressionType(expressionTypeEnumType.getType());
+            expressionTypes.add(expressionType);
+        }
+
+        try {
+            expressionTypeRepository.saveAll(expressionTypes);
+        } catch (Exception e) {
+            log.info("Duplicate Not Allowed");
 
 
-     
+        }
 
     }
 
 
 }
+
