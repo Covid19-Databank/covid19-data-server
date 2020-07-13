a905af988dfc29831f42ae3d1bfd64bbaced0f02
Vicad006 <vicad006@yaoo.com>
2020-07-13 20:26:30 +0100

    Added @Repository annotation to LiteratureTypeRepository

--- src/main/java/org/covid19databank/repository/LiteratureTypeRepository.java
+++ src/main/java/org/covid19databank/repository/LiteratureTypeRepository.java
@@ -1,6 +1,6 @@
 package org.covid19databank.repository;
 
-import org.covid19databank.datamodel.LiteratureType;
+import org.covid19databank.datamodel.literatures.LiteratureType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
